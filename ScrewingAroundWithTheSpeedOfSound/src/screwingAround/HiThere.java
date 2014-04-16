bffd90b17fb5cd6d72ec40aff24d374be55feefd
GabrielWB <gwbeljaars@gmail.com>
2014-04-16 10:15:57 +0200

    Init

---
+++ ScrewingAroundWithTheSpeedOfSound/src/screwingAround/HiThere.java
@@ -0,0 +1,13 @@
+package screwingAround;
+
+public class HiThere {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//If you can read this, then things have worked out as they were supposed to.
+		System.out.println("Gotta go fast!");
+
+	}
+
+}
