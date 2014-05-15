a2ddfdc318311dce15da735497dc6e41b2b19bc0
GabrielWB <gwbeljaars@gmail.com>
2014-05-15 16:16:37 +0200

    testcommit

--- ScrewingAroundWithTheSpeedOfSound/src/screwingAround/HiThere.java
+++ ScrewingAroundWithTheSpeedOfSound/src/screwingAround/HiThere.java
@@ -2,14 +2,17 @@ package screwingAround;
 
 public class HiThere {
 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 		
 		//If you can read this, then things have worked out as they were supposed to.
 		//If only this worked without problems...
 		System.out.println("Gotta go fast!");
 		
 		//nom2
+		//li
 		
 		//nom3
 
