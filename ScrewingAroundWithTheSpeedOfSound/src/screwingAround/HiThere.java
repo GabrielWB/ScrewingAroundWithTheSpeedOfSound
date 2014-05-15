92ced5f2877b640fff0d2a0136bb90a6a8eda51a
GabrielWB <gwbeljaars@gmail.com>
2014-05-15 16:05:33 +0200

    nom2

--- ScrewingAroundWithTheSpeedOfSound/src/screwingAround/HiThere.java
+++ ScrewingAroundWithTheSpeedOfSound/src/screwingAround/HiThere.java
@@ -8,6 +8,8 @@ public class HiThere {
 		//If you can read this, then things have worked out as they were supposed to.
 		//If only this worked without problems...
 		System.out.println("Gotta go fast!");
+		
+		//nom2
 
 	}
 
